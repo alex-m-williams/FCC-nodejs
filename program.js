830bbd3a2638e8721332cf3c38f90fd3b9b65853
Alex Williams <alexwilliams567@gmail.com>
2017-11-27 21:10:21 +0000

    command line args

--- program.js
+++ program.js
@@ -1,1 +1,5 @@
-console.log("HELLO WORLD");
+let arr = process.argv.slice(2, process.argv.length);
+console.log(arr.reduce((sum, int) => {
+    let sumAdd = Number(sum);
+    return sumAdd += Number(int); 
+}));
